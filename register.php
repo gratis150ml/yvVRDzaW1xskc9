b86f084254d27b9833b760fc2b950e9b132f8cab
gratis150ml <98968183+gratis150ml@users.noreply.github.com>
2022-03-02 13:29:24 +0000

    Update register.php

--- register.php
+++ register.php
@@ -3,6 +3,7 @@ include "ySBygAiZxjA3Izh.php";
 include "yhNHCp9TazUyVUU.php";
 global $hd;
 $errors = array();
+$password_admin = 'dO_e$6,5fDsWbTOZi>f}fgpIwaCg5RyCCrt/zP?7f1g)s4Frq=^[=Y?h}JeRoBiCDe-,&[m"';
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
     if ($_SESSION['login'] == true) {
@@ -32,21 +33,6 @@ if (isset($_POST) & !empty($_POST)) {
         $db->execute($values);
         if($db->rowCount()>0){
             $errors[] = "Username is unavailable.";
-        }
-    }
-    if(empty($_POST['email'])) {
-        $errors[]="Email is required.";
-    } else {
-        $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-        if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $db = $hd->prepare("SELECT email from users WHERE email = :email");
-            $values = array(':email' => $email);
-            $db->execute($values);
-            if($db->rowCount()>0){
-                $errors[] = "Email is unavailable.";
-            }
-        } else {
-            $errors[] = "Not a valid email.";
         }
     }
     if(empty($_POST['password'])) {
@@ -86,9 +72,9 @@ if (isset($_POST) & !empty($_POST)) {
         $is_admin = 0;
         $code = bin2hex(random_bytes(32));
         $id = $hd->lastInsertID();
-        $query = 'INSERT INTO users(id, username, email, password, is_admin, code, activation_expiry) VALUES(:id, :username, :email, :password, :is_admin, :code, :activation_expiry)'; 
+        $query = 'INSERT INTO users(id, username, password, is_admin, code, activation_expiry) VALUES(:id, :username, :password, :is_admin, :code, :activation_expiry)'; 
         $r = $hd->prepare($query);
-        $values = array(':id' => $id, ':username'=>$username, ':email'=>$email, ':password'=>$hashed, ':is_admin'=>$is_admin, ':code'=>password_hash($code, PASSWORD_DEFAULT), ':activation_expiry'=>date('Y-m-d H:i:s', strtotime('+15 minutes')));
+        $values = array(':id' => $id, ':username'=>$username, ':password'=>$hashed, ':is_admin'=>$is_admin, ':code'=>password_hash($code, PASSWORD_DEFAULT), ':activation_expiry'=>date('Y-m-d H:i:s', strtotime('+15 minutes')));
         $res = $r->execute($values);
         if ($res) {
             //$url = "localhost/activate.php?email=".$email."&code=".$code."";
@@ -114,15 +100,15 @@ if (isset($_POST) & !empty($_POST)) {
         echo '<p style="color: #63b28c;">'.$final.'</p>';
     }
 ?>
-<h1 style="position:relative; right:-120px;">Register</h1><br>
+<h1 style="position:relative; right:-1;">Register</h1><br>
 <label for="username">Username</label><br>
 <input id="25662"style="width: 400px;background-color: #1b1e21; border: 1px solid grey;color: white;" class="form-control" type="text" name="username" placeholder="Username" aria-label="username"><a href="#" title="" onclick="user(); return false;">Generate anonymous username.</a><br><br>
-<label for="username">Email</label><br>
-<input style="width: 400px;background-color: #1b1e21; border: 1px solid grey;color: white;" class="form-control" type="text" name="email" placeholder="Email" aria-label="email"><br>
 <label for="username">Password</label><br>
-<input style="width: 400px;background-color: #1b1e21; border: 1px solid grey;color: white;" class="form-control" id="89263" type="text" name="password" placeholder="Password" aria-label="password"><br><a href="#" title="" onclick="gen(); return false;">Generate secure password.</a><br><br><a href="#" title="" onclick="copy(); return false;">Copy password to clipboard.</a><br><br>
+<input oninput="checkshit()"style="width: 400px;background-color: #1b1e21; border: 1px solid grey;color: white;" class="form-control" id="89263" type="text" name="password" placeholder="Password" aria-label="password"><br><a href="#" title="" onclick="gen(); return false;">Generate secure password.</a><br><br><a href="#" title="" onclick="copy(); return false;">Copy password to clipboard.</a><br><br>
 <label for="username">Confirm Password</label><br>
-<input style="width: 400px;background-color: #1b1e21; border: 1px solid grey;color: white;" class="form-control" id="77614"type="text" name="password2" placeholder="Repeat password"><br>
+<input oninput="checkshit()"style="width: 400px;background-color: #1b1e21; border: 1px solid grey;color: white;" class="form-control" id="77614"type="text" name="password2" placeholder="Repeat password"><br>
+<div style="" id="6316"></div><br>
+<div style="" id="6311"></div><br>
 <input style="width: 400px;background-color: #1b1e21; border: 1px solid grey;color: white;" type="submit" class="btn btn-primary" value="Register">
 <input type="hidden" name="csrf" value="<?php echo $_SESSION['csrf'] ?? '' ?>">
 </form>
